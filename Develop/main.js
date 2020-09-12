83ac08b17cf66cd51cfddfdf8521af4f3244e4d5
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-09-13 00:55:31 +0930

    final html and design of day plannerfinished minus colours but will be adding colour through jQuery

---
+++ Develop/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    //send the current date to top of the page
+    let nowMoment = moment().format('dddd Do MMMM YYYY');
+    $("#currentDay").append(nowMoment);
+
+
+
+})
+
+
+
+
+
+
